217d2fc81f17da19770420b9791e51aa3ff91d52
毕殿卿 <bdq123@outlook.com>
2017-11-08 21:35:35 +0800

    添加项目文件。

---
+++ DateTime_Local/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace DateTime_Local
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo swedishCulture = new CultureInfo("sv-SE");
+            DateTimeFormatInfo swedishDateFormat = swedishCulture.DateTimeFormat;
+
+            CultureInfo hungarianCulture = new CultureInfo("hu-HU");
+            DateTimeFormatInfo hungarianDateFormat = hungarianCulture.DateTimeFormat;
+
+            CultureInfo germanCulture = new CultureInfo("de-DE");
+            DateTimeFormatInfo germanDateFormat = germanCulture.DateTimeFormat;
+
+            CultureInfo zhcnCulture = new CultureInfo("zh-CN");
+            DateTimeFormatInfo zhcnDateFormat = zhcnCulture.DateTimeFormat;
+
+            DateTime now = DateTime.Now;
+
+            string formattedDateSweden = now.ToString(swedishDateFormat.FullDateTimePattern);
+            string formattedDateHungary = now.ToString(hungarianDateFormat.FullDateTimePattern);
+            string formattedDateGermany = now.ToString(germanDateFormat.FullDateTimePattern);
+            string formattedDateZhcn = now.ToString(zhcnDateFormat.FullDateTimePattern);
+
+            Console.WriteLine(formattedDateSweden);
+            Console.WriteLine(formattedDateHungary);
+            Console.WriteLine(formattedDateGermany);
+            Console.WriteLine(formattedDateZhcn);
+
+            Console.ReadKey();
+        }
+    }
+}
